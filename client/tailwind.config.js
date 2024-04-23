4f4f22f717f98f64e182eded1747f999315e15a2
VladimirMedv <167883925+VladimirMedv@users.noreply.github.com>
2024-04-23 19:25:55 -0300

    create user model

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,15 +2,9 @@ const flowbite = require("flowbite-react/tailwind");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(), // Добавляем путь к содержимому Flowbite
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
     extend: {},
   },
-  plugins: [
-    flowbite.plugin(), // Добавляем плагин Flowbite
-  ],
+  plugins: [flowbite.plugin()],
 };
